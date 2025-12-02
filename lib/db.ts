a05e17a5adcf82c3e9d16a550c4a62afec85a839
Versha Verma <vershuverma02@gmail.com>
2025-12-03 00:10:16 +0530

    Fix DB config

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,14 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'your_username',
-  password: 'your_password',
-  database: 'school_db',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: Number(process.env.MYSQL_PORT),
+  ssl: {
+    rejectUnauthorized: true,
+  }
 });
 
 export default db;
